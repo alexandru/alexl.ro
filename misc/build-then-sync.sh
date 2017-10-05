85e153fa29c84a91c20f744e31247e9b59d4efbf
Alexandru Nedelcu <noreply@alexn.org>
2017-10-05 17:34:30 +0300

    Fix

--- misc/build-then-sync.sh
+++ misc/build-then-sync.sh
@@ -7,4 +7,6 @@ mkdir -p $HOME/.ssh
 chmod go-rwx $HOME/.ssh
 cp misc/travis_rsa $HOME/.ssh
 
-rsync --delete-excluded -Pacv -e "ssh -i $HOME/.ssh/travis_rsa" build/ alex@alexn.org:/var/www/alexl.ro/
+rsync --delete-excluded -Pacv \
+  -e "ssh -o 'StrictHostKeyChecking no' -i $HOME/.ssh/travis_rsa" \
+  build/ alex@alexn.org:/var/www/alexl.ro/
